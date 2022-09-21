e8e1ac2309c2cd98ec4b79f460840a7eab27a2a9
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-09-21 20:08:18 +0100

    NCI Chess Club
    Web Design Final Project 2020

---
+++ js/cookies.js
@@ -0,0 +1,16 @@
+		<!-- cookies script -->
+		<!-- Creator cookies from https://www.osano.com -->
+		window.cookieconsent.initialise({
+		  "palette": {
+			"popup": {
+			  "background": "#237afc"
+			},
+			"button": {
+			  "background": "#fff",
+			  "text": "#237afc"
+			}
+		  },
+		  "theme": "edgeless",
+		  "type": "opt-out"
+		});
+		
